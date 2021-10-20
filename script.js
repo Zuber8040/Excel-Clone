ad8f2034cf6c75d14821b3af38c7eb9faddcbc13
Zuber8040 <zuberustad456@gmail.com>
2021-10-20 09:41:56 +0530

    6th day

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ let cols=26;
 
 let addressColConst = document.querySelector(".address-col-cont");
 let addressRowConst = document.querySelector(".address-row-cont");
-let cellsCont = document.querySelector(".cells-cont")
+let cellsCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar")
+
+
+
 for(let i=0;i<rows;i++){
     let addressCol = document.createElement("div");
     addressCol.setAttribute("class","address-col");
 
     addressCol.innerText = i+1; 
     addressColConst.appendChild(addressCol);
-
-
-
 }
-
 for(let i=0;i<cols;i++){
     let addressRow = document.createElement("div");
     addressRow.setAttribute("class","address-row");
@@ -28,9 +28,18 @@ for(let i=0;i<rows;i++){
     rowCont.setAttribute("class","row-cont")
     for(let j=0;j<cols;j++){
         let cell = document.createElement("div");
-        cell.setAttribute("class","ceil");
+        cell.setAttribute("class","cell");
+        cell.setAttribute("contenteditable","true");
         rowCont.appendChild(cell);
-    }
+        addListenerForAddressBarDisplay(cell,i,j);
+    } 
     cellsCont.appendChild(rowCont);
 
+}
+function  addListenerForAddressBarDisplay(cell,i,j){
+    cell.addEventListener("click", (e) => {    
+        let rowID =i+1;
+        let colID = String.fromCharCode(65+j);
+        addressBar.value=`${colID}${rowID}`;
+    });
 }
