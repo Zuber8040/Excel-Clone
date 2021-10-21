53a2168600bbc8f7b11aab0056c45d654dd604c7
Zuber8040 <zuberustad456@gmail.com>
2021-10-21 14:03:51 +0530

    7th day

--- cell-properties.js
+++ cell-properties.js
@@ -1,15 +1,124 @@
 // Storage 
 // in this we will see that if user c=click itlaic ,bold,and evry cell functions
 
+// 2 way binding menas 1ts change in data /storage then 2nd is UI change 
+
+
 let sheetDB = [];  // it stored all the actions 
+
 
 for(let i=0;i<rows;i++){
     let sheetRow =[];  // it stored every cell obj stored here
     for(let j=0;j<cols;j++){
+
+        // properties of cell 
         let cellProp={
-            
+            bold: false,
+            italic :false,
+            underline:false,
+            alignment :"left",
+            fontFamily : "monospace",
+            fontSize :"14",
+            fontColor:"#000000",
+            BGcolor :"#000000",  // Just for indication purpose 
         }
         sheetRow.push(cellProp);
     }   
     sheetDB.push(sheetRow);
 }
+
+// Selectors for cell properties   
+let bold = document.querySelector(".bold");
+let italic = document.querySelector(".italic");
+let underline = document.querySelector(".underline");
+let fontSize = document.querySelector(".font-size-prop");
+let fontFamily = document.querySelector(".font-family-prop");
+let fontColor = document.querySelector(".font-color-prop");
+let BGcolor = document.querySelector(".BGcolor-prop");
+let alignment = document.querySelectorAll(".alignment");
+let leftAlign = alignment[0];
+let centerAlign = alignment[1];
+let rightAlign = alignment[2];
+
+
+let activeColorProp = "#d1d8e0";
+let inactiveColorProp = "#ecf0f1";
+
+
+bold.addEventListener("click", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+
+    // Modification
+    cellProp.bold = !cellProp.bold; // Data change
+    cell.style.fontWeight = cellProp.bold ? "bold" : "normal"; // UI change (1)
+    bold.style.backgroundColor = cellProp.bold ? activeColorProp : inactiveColorProp; // UI change (2)
+})
+italic.addEventListener("click", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+
+    // Modification
+    cellProp.italic = !cellProp.italic; // Data change
+    cell.style.fontStyle = cellProp.italic ? "italic" : "normal"; // UI change (1)
+    italic.style.backgroundColor = cellProp.italic ? activeColorProp : inactiveColorProp; // UI change (2)
+})
+
+underline.addEventListener("click", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+
+    // Modification
+    cellProp.underline = !cellProp.underline; // Data change
+    cell.style.textDecoration = cellProp.underline ? "underline" : "none"; // UI change (1)
+    underline.style.backgroundColor = cellProp.underline ? activeColorProp : inactiveColorProp; // UI change (2)
+})
+fontSize.addEventListener("change", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+
+    cellProp.fontSize = fontSize.value; // Data change
+    cell.style.fontSize = cellProp.fontSize + "px";
+    fontSize.value = cellProp.fontSize;
+})
+fontFamily.addEventListener("change", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+
+    cellProp.fontFamily = fontFamily.value; // Data change
+    cell.style.fontFamily = cellProp.fontFamily;
+    fontFamily.value = cellProp.fontFamily;
+})
+fontColor.addEventListener("change", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+
+    cellProp.fontColor = fontColor.value; // Data change
+    cell.style.color = cellProp.fontColor;
+    fontColor.value = cellProp.fontColor;
+})
+BGcolor.addEventListener("change", (e) => {
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+
+    cellProp.BGcolor = BGcolor.value; // Data change
+    cell.style.backgroundColor = cellProp.BGcolor;
+    BGcolor.value = cellProp.BGcolor;
+})
+
+function getCellAndCellProp(address){
+    let [rid, cid] = decodeRIDCIDFromAddress(address);
+    // Access cell & storage object
+    let cell = document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+    let cellProp = sheetDB[rid][cid];
+    return [cell, cellProp];
+
+}
+function decodeRIDCIDFromAddress(address) {
+    // address -> "A1"
+    let rid = Number(address.slice(1) - 1); // "1" -> 0
+    let cid = Number(address.charCodeAt(0)) - 65; // "A" -> 65
+    return [rid, cid];
+}
+
+
--- grid.js
+++ grid.js
@@ -8,13 +8,18 @@ let addressBar = document.querySelector(".address-bar")
 
 
 
-for(let i=0;i<rows;i++){
+//rows representationfor 1 to 100
+
+for(let i=0;i<rows;i++){   
     let addressCol = document.createElement("div");
     addressCol.setAttribute("class","address-col");
 
     addressCol.innerText = i+1; 
     addressColConst.appendChild(addressCol);
 }
+
+//colum representaion for A to Z
+
 for(let i=0;i<cols;i++){
     let addressRow = document.createElement("div");
     addressRow.setAttribute("class","address-row");
@@ -23,6 +28,8 @@ for(let i=0;i<cols;i++){
     addressRowConst.appendChild(addressRow);
 }
 
+
+// inside the rows and coloums
 for(let i=0;i<rows;i++){
     let rowCont = document.createElement("div");
     rowCont.setAttribute("class","row-cont")
@@ -30,12 +37,18 @@ for(let i=0;i<rows;i++){
         let cell = document.createElement("div");
         cell.setAttribute("class","cell");
         cell.setAttribute("contenteditable","true");
+        cell.setAttribute("spellcheck","false")   // checking the spelling 
+        // Attributes for cell and storage idenfication 
+        cell.setAttribute("rid",i);  
+        cell.setAttribute("cid",j);
+
         rowCont.appendChild(cell);
         addListenerForAddressBarDisplay(cell,i,j);
     } 
     cellsCont.appendChild(rowCont);
 
 }
+// if user click on the box the it will show which row and coloum u  have clicked function 
 function  addListenerForAddressBarDisplay(cell,i,j){
     cell.addEventListener("click", (e) => {    
         let rowID =i+1;
@@ -43,3 +56,9 @@ function  addListenerForAddressBarDisplay(cell,i,j){
         addressBar.value=`${colID}${rowID}`;
     });
 }
+
+// by default click on first cell via DOM 
+let firstCell = document.querySelector(".cell");
+firstCell.click();
+
+
