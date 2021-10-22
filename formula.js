eac395b99a5afe7e186fef1bc17a9b7d78b27810
Zuber8040 <zuberustad456@gmail.com>
2021-10-22 22:46:54 +0530

    8th end

--- formula.js
+++ formula.js
@@ -1,6 +1,5 @@
 
 
-
 for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
         let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);
@@ -9,27 +8,39 @@ for (let i = 0; i < rows; i++) {
             let [activeCell, cellProp] = getCellAndCellProp(address);
             let enteredData = activeCell.innerText;
             cellProp.value=enteredData;
-           
+            
         })
     }
-}
+}   
+
+
 let formulaBar = document.querySelector(".formula-bar");
-formulaBar.addEventListener("keydown", async (e) => {
+formulaBar.addEventListener("keydown",  (e) => {
     let inputFormula = formulaBar.value;
     if (e.key === "Enter" && inputFormula) {
 
-        let evaluatedValue = evaluateformula(inputFormula);
+        let evaluatedValue = evaluateFormula(inputFormula);
 
         // To update UI and cellProp in DB
-        setCellUIAndCellProp(evaluatedValue, inputFormula, address);
+        setCellUIAndCellProp(evaluatedValue, inputFormula);
     }
 })
 
-function evaluateformula(formula){
-    return eval(formula);
+function evaluateFormula(formula){
+    let encodedFormula = formula.split(" ");
+    for (let i = 0; i < encodedFormula.length; i++) {
+        let asciiValue = encodedFormula[i].charCodeAt(0);
+        if (asciiValue >= 65 && asciiValue <= 90) {
+            let [cell, cellProp] = getCellAndCellProp(encodedFormula[i]);
+            encodedFormula[i] = cellProp.value;
+        }
+    }
+    let decodedFormula = encodedFormula.join(" ");
+    return eval(decodedFormula);
 }
 
-function setCellUIAndCellProp(evaluatedValue, formula, address) {
+function setCellUIAndCellProp(evaluatedValue, formula) {
+    let address = addressBar.value;
     let [cell, cellProp] = getCellAndCellProp(address);
 
     //UI update
@@ -37,4 +48,4 @@ function setCellUIAndCellProp(evaluatedValue, formula, address) {
     // DB update
     cellProp.value = evaluatedValue;
     cellProp.formula = formula;
-}        
+}    
